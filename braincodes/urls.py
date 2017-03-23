0b0e3b485c0443864a330345c6cf38def6c21899
ravit <ravit61294@gmail.com>
2017-03-23 14:08:04 +0530

    Initial Commit

--- braincodes/urls.py
+++ braincodes/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$' , views.index,name='index') ]
+    url(r'^$' , views.index,name='index'),
+    url(r'^compile-and-run/$', views.compile_and_run),
+    ]
